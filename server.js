cc28699f197cf180c65637817a7535de5c52f267
rockerahul97 <rockerahul97@gmail.com>
2017-08-16 20:46:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ app.get('/article-one',function (req,res){
 });
 
 app.get('/article-two',function (req,res){
-    res.send("This is 2nd article and it is requested and served here");
+    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/article-three',function (req,res){
-    res.send("This is 3rd article and it is requested and served here");
+    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
