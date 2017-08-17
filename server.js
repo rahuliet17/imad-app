51cf41510a3cc11a77cf5de653df2e657e679334
rockerahul97 <rockerahul97@gmail.com>
2017-08-18 01:42:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -95,7 +95,8 @@ app.get('/:articleName',function (req,res){
   res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/thankyou',function (req,res){
+app.get('/:articleName',function (req,res){
+    var articleName=req.params.articleName;
     res.sendFile(path.join(__dirname, 'ui', 'thankyou.html'));
 });
 
