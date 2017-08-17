a5b4d3fa44f75a67c98a7ccf0db2fcb7faef2431
rockerahul97 <rockerahul97@gmail.com>
2017-08-17 20:00:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,6 +21,10 @@ app.get('/article-three',function (req,res){
     res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
+app.get('/thankyou',function (req,res){
+    res.sendFile(path.join(__dirname, 'ui', 'thankyou.html'));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
