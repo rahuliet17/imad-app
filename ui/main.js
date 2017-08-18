519a3dc80382d26fbc4a163b23247d344fb3da59
rockerahul97 <rockerahul97@gmail.com>
2017-08-18 13:51:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -36,7 +36,7 @@ button.onclick = function (){
     };
     
     //Make the request
-    request.open('GET',"http://rockerahul97.imad.hasura-app.io/counter",true);
+    request.open('GET','http://rockerahul97.imad.hasura-app.io/counter',true);
     request.send(null);
 };
     //Render the variable in the correct span
