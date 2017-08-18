048704390f62eb96045161e67b8cde3a7f344f0a
rockerahul97 <rockerahul97@gmail.com>
2017-08-18 13:56:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,7 +17,7 @@ img.onclick = function(){
 
 //Counter Code
 var button = document.getElementById('counter');
-//var counter=0;
+
 button.onclick = function (){
     
     //Create a request object
