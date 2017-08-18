719f9bed62d5be5e819b316e62e38a4afff58da5
rockerahul97 <rockerahul97@gmail.com>
2017-08-18 14:40:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,22 +1,6 @@
-/*console.log('Loaded!');
-
-//Changing the text of main-text div
-var element = document.getElementById('main-text');
-element.innerHTML = "Rahul";
-
-//Move the Image
-var img = document.getElementById('madi');
-var marginLeft=0;
-function moveRight(){
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function(){
-    var interval = setInterval(moveRight,50);
-};*/
-
 //Counter Code
 var button = document.getElementById('counter');
+
 button.onclick = function (){
     
     //Create a request object
@@ -39,8 +23,3 @@ button.onclick = function (){
     request.open('GET', 'http://rockerahul97.imad.hasura-app.io/counter', true);
     request.send(null);
 };
-    //Render the variable in the correct span
-/*  counter = counter + 1;
-  var span = document.getElementById('count');
-  span.innerHTML = counter.toString();
-};*/
