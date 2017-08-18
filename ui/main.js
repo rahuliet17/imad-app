0e8035f6ca301c71104632362132f20eb93f77b3
rockerahul97 <rockerahul97@gmail.com>
2017-08-18 13:37:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
-console.log('Loaded!');
+/*console.log('Loaded!');
 
 //Changing the text of main-text div
 var element = document.getElementById('main-text');
@@ -13,4 +13,19 @@ function moveRight(){
 }
 img.onclick = function(){
     var interval = setInterval(moveRight,50);
+};*/
+
+//Counter Code
+var button = document.getElementById('counter');
+var counter=0;
+button.onclick = function (){
+    
+    //Make a request to counter endpoint
+    
+    //Capture the response and store it in a variable
+    
+    //Render the variable in the correct span
+  counter = counter + 1;
+  var span = document.getElementById('count');
+  span.innerHTML = counter.toString();
 };
