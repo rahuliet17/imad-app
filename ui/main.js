9c8ddb4b61ce933dd108b7c27928a4b4789917cb
rockerahul97 <rockerahul97@gmail.com>
2017-08-18 17:00:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,27 +1,25 @@
 //Counter Code
 //var button = document.getElementById('b-count');
-/*button.onclick = */function exec(){
-    
-    //Create a request object
+//button.onclick = function exec(){
+function exec(){
     var request = new XMLHttpRequest();
-    
-    //Capture the response and store it in a variable
-    request.onreadtstatechange = function () { 
-        if(request.readyState === XMLHttpRequest.DONE){
-            //Take some action
-            if(request.status === 200) {
-                var counter = request.responseText;
-                var span = document.getElementById('s-count');
-                span.innerHTML = counter.toString();
-            }
-        }
-        //Not done yet
-    };
-    
-    //Make the request
-    request.open('GET', 'http://rockerahul97.imad.hasura-app.io/counter', true);
-    request.send(null);
-};
+    request.onreadystatechange = function() 
+    {
+          if (request.readyState === XMLHttpRequest.DONE) 
+          {
+              //Take Some Action
+              if (request.status === 200) 
+                { 
+                    var counter = request.responseText; 
+                    var span = document.getElementById('count');
+                    span.innerHTML = counter.toString();
+                } 
+          } 
+    }; 
+    request.open("GET", "http://rockerahul97.imad.hasura-app.io/counter", true);
+    request.send(); 
+    console.log('EXECUTED');
+}
 /*function exec(){
     var request = new XMLHttpRequest();
     request.onreadystatechange = function() 
