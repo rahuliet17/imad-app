f5251e217f6b0d77f3ebf1802cf9c17f231c6add
rockerahul97 <rockerahul97@gmail.com>
2017-08-18 17:03:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,4 @@
 //Counter Code
-//var button = document.getElementById('b-count');
-//button.onclick = function exec(){
 function exec(){
     var request = new XMLHttpRequest();
     request.onreadystatechange = function() 
@@ -20,22 +18,3 @@ function exec(){
     request.send(); 
     console.log('EXECUTED');
 }
-/*function exec(){
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function() 
-    {
-          if (request.readyState === XMLHttpRequest.DONE) 
-          {
-              //Take Some Action
-              if (request.status === 200) 
-                { 
-                    var counter = request.responseText; 
-                    var span = document.getElementById('count');
-                    span.innerHTML = counter.toString();
-                } 
-          } 
-    }; 
-    request.open("GET", "http://rockerahul97.imad.hasura-app.io/counter", true);
-    request.send(); 
-    console.log('EXECUTED');
-}*/
