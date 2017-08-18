899ed28f83da6dae578a6266c77f8699efb6f0af
rockerahul97 <rockerahul97@gmail.com>
2017-08-18 12:16:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var element = document.getElementById('main-text');
 element.innerHTML = "Rahul";
 
 //Move the Image
-var img = document.getElementById('madi');
+var img = document.getElementById('img');
 img.onclick = function(){
     img.style.marginLeft = '100px';
 }
