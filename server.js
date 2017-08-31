17f2054b570af30517301b6099d134c3b76ed1e1
rockerahul97 <rockerahul97@gmail.com>
2017-08-31 20:16:46 +0000

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -119,7 +119,7 @@ app.post('/login', function(req,res){
                 var salt = dbString.split('$')[2];
                 var hashedPassword = hash(password,salt);
                 if(hashedPassword === dbString){
-                    req.params.auth = {userId: result.rows[0].id};
+                    req.session.auth = {userId: result.rows[0].id};
                     res.send('credentials correct :)');
                 }else
                 {
